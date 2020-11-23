6c4988eb483647ed216e7fccf9a00e4be18b7b89
Asher Mathews Shaji <msasher123@gmail.com>
2020-11-23 16:43:00 +0530

    Added SearchBar

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 var searchbutton = document.querySelector(".search");
 var searchfield = document.querySelector(".searchinput");
+var searchquery = "";
+var searchfind = false;
+var listofsoftware = document.querySelectorAll(".software");
 
 searchbutton.addEventListener("click", function () {
   var searchquery = searchfield.value;
-  console.log(searchquery);
+  //   console.log(searchquery);
+
+  for (var i = 0; i < listofsoftware.length; ++i) {
+    //   console.log(listofsoftware[i].innerHTML);
+    if (searchquery == listofsoftware[i].innerHTML) {
+      console.log(i);
+      searchfind = true;
+    } else {
+      listofsoftware[i].classList.add("hidden");
+    }
+    if (!searchfind) {
+      document.querySelector(".nomessage").innerHTML = "SoftWARE Not FOund";
+    }
+  }
 });
 
-var listofsoftware = document.querySelectorAll(".software");
-console.log(listofsoftware);
-
-for (var i = 0; i < listofsoftware.length; ++i) {
-  console.log(listofsoftware[i].innerHTML);
-}
+// console.log(listofsoftware);
